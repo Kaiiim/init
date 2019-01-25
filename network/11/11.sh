c42c6af1b18beec3594aa918ab3e064ae757f66c
Karim KHALFAOUI <kkhalfao@e2r5p9.42.fr>
2019-01-25 11:33:03 +0100

    exo 12 ok

---
+++ network/11/11.sh
@@ -0,0 +1,6 @@
+# 'nat' stands for 'Network Address Translation'. nat1 is the device that makes the link between local network and outside world
+
+
+nat-1\nip:   10.60.1.11
+
+
---
+++ network/12/12.sh
@@ -0,0 +1,6 @@
+# Trouvez l’IP qui vous a été assignée par le serveur dhcp
+
+# https://www.culture-informatique.net/cest-quoi-un-serveur-dhcp-niv1/
+# ipconfig getpacket en0
+
+
