ef5001ce8ebc3154b84194ccfcb784331cb1426d
Karim KHALFAOUI <kkhalfao@e2r2p18.42.fr>
2019-01-25 13:08:17 +0100

    exo 15 ok

--- network/12/12.sh
+++ network/12/12.sh
@@ -3,4 +3,8 @@
 # https://www.culture-informatique.net/cest-quoi-un-serveur-dhcp-niv1/
 # ipconfig getpacket en0
 
+ipconfig getpacket en0 | grep "server_identifier" | awk '{print $3}'
+nslookup 10.51.1.253
+
+# Check that the server with the 10.51.1.253 IP address is reachable from your computer
 
---
+++ network/13/13.sh
@@ -0,0 +1,3 @@
+# Grâce a la question précédente et au reverse DNS retrouvez le nom de votre host
+
+nslookup 10.51.1.253
